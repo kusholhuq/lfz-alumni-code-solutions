721c87e54f1988ca60e2bb14dde583af3cb777fa
Mohsinul Huq <kusholhuq@gmail.com>
2021-08-31 22:37:52 -0700

    Kushol Huq | JS Variables and Primitives

---
+++ exercises/js-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = "Kushol Huq";
+var isCool = false;
+var totalPets = 1;
+
+console.log("fullName: ", fullName);
+console.log("isCool: ", isCool);
+console.log("totalPets: ", totalPets);
+
+console.log("typeof fullName: ", typeof fullName);
+console.log("typeof isCool: ", typeof isCool);
+console.log("typeof totalPets: ", typeof totalPets);
+
+var unknown = null;
+console.log("unknown: ", unknown);
+console.log("unknown typeof: ", typeof unknown);
+
+var never;
+console.log("never: ", never);
+console.log("never typeof: ", typeof never);
