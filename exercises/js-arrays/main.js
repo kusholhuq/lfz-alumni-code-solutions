d1eb031e9dfbf87502cdd1d52e12cbe9c31f6933
Mohsinul Huq <kusholhuq@gmail.com>
2021-09-06 21:25:40 -0700

    string concat

--- exercises/js-arrays/main.js
+++ exercises/js-arrays/main.js
@@ -2,3 +2,4 @@ var colors = ['red', 'white', 'blue'];
 console.log(colors[0]);
 console.log(colors[1]);
 console.log(colors[2]);
+console.log("America is " + colors[0] +", "+ colors[1] + ", and " + colors[2] +".");
