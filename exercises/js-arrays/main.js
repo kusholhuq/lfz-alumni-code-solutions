a0764c223fb35e5973dc81b7a619fb00cdb75539
Mohsinul Huq <kusholhuq@gmail.com>
2021-09-07 18:25:13 -0700

    Kushol Huq | Javascript Arrays

--- exercises/js-arrays/main.js
+++ exercises/js-arrays/main.js
@@ -1,5 +1,15 @@
 var colors = ['red', 'white', 'blue'];
-console.log(colors[0]);
-console.log(colors[1]);
-console.log(colors[2]);
+console.log("colors[0]: ",colors[0]);
+console.log("colors[1]: ",colors[1]);
+console.log("colors[2]: ",colors[2]);
 console.log("America is " + colors[0] +", "+ colors[1] + ", and " + colors[2] +".");
+colors[2] = 'green';
+console.log("colors: ", colors)
+
+var students = ['Kushol', 'Cherie', 'Uzair', 'TJ'];
+var numberOfStudents = students.length;
+console.log("numberOfStudents: ", numberOfStudents);
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+console.log("lastStudent: ", lastStudent);
+console.log("students: ", students);
