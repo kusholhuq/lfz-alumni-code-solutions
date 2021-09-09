3d3a826326fbb8e62642d728796d8d2ab5d7dca5
Mohsinul Huq <kusholhuq@gmail.com>
2021-09-08 17:28:04 -0700

    Kushol Huq | Javascript Data Model

---
+++ exercises/js-data-model/order-history.js
@@ -0,0 +1,68 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: 34,
+    shipTo: 'JS Masher',
+    orderNumber: 11439416898772232,
+    deliveryDate: 'Aug 8, 2020',
+    products: [
+      {
+        productName: 'JavaScript for impatient programmers',
+        productAuthor: 'Raschmayer, Dr. Axel',
+        returnWindow: 'Sep 7, 2020',
+        productPrice: 31.55
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: 11399842681280257,
+    deliveryDate: 'Jul 20, 2020',
+    products: [
+      {
+        productName: 'The Timeless Way of Building',
+        productAuthor: 'Alexander, Christoper',
+        returnWindow: 'Aug 19, 2020',
+        productPrice: 41.33
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: 11428755579059409,
+    deliveryDate: 'Jul 7, 2020',
+    products: [
+      {
+        productName: 'Gamecube Controller Adapter...',
+        productAuthor: '',
+        returnWindow: 'Aug 5, 2020',
+        productPrice: 15.98
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: 11328831772648248,
+    deliveryDate: 'Jul 5, 2020',
+    products: [
+      {
+        productName: 'Gamecube Controller - Super...',
+        productAuthor: '',
+        returnWindow: 'Aug 4, 2020',
+        productPrice: 94.95
+      },
+      {
+        productName: 'The Art of Sql',
+        productAuthor: 'Faroult, Stephane',
+        returnWindow: 'Aug 4, 2020',
+        productPrice: 33.99
+      }
+    ]
+  }
+];
