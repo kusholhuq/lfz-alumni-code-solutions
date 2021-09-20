93aba35c122fd310b662317999e24afebdd6ff23
Mohsinul Huq <kusholhuq@gmail.com>
2021-09-19 19:55:50 -0700

    add suffix

--- exercises/js-loops-practice/add-suffix-to-all.js
+++ exercises/js-loops-practice/add-suffix-to-all.js
@@ -1,1 +1,8 @@
 /* exported addSuffixToAll */
+function addSuffixToAll(words, suffix){
+  const appendedWords = [];
+  for(let i=0; i<words.length; i++){
+    appendedWords.push(words[i]+suffix);
+  }
+  return appendedWords;
+}
