c172914963026d5132348f304be2d94fa5cde088
Mohsinul Huq <kusholhuq@gmail.com>
2021-09-27 20:52:47 -0700

    getWords reverseWord

--- exercises/string-manipulation-1/src/reverse-word.js
+++ exercises/string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,8 @@
 /* exported reverseWord */
+function reverseWord(string){
+  let reversedWord = '';
+  for(let i=string.length-1; i>=0; i--){
+    reversedWord+=string[i];
+  }
+  return reversedWord;
+}
