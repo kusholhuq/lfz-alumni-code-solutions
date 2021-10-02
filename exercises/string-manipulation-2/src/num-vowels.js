0b0337815d7949e0d9f4e1f472b4d732ffd5dce8
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-02 13:07:22 -0700

    numVowels

--- exercises/string-manipulation-2/src/num-vowels.js
+++ exercises/string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,17 @@
 /* exported numVowels */
+function numVowels(string){
+let count = 0;
+for(let i=0; i<string.length;i++){
+  switch(string[i].toLowerCase()){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      count++;
+      break;
+    default:
+  }
+}
+return count;
+}
