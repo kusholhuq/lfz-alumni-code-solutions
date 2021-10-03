e1d26b68d99ee149a5f262823bbd0d195a0cb482
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-03 12:51:44 -0700

    lastChars

--- exercises/string-manipulation-2/src/last-chars.js
+++ exercises/string-manipulation-2/src/last-chars.js
@@ -1,1 +1,11 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  if(length > string.length){
+    return string;
+  }
+  let result = '';
+  for (let i = string.length - length; i < string.length; i++) {
+    result += string[i];
+  }
+  return result;
+}
