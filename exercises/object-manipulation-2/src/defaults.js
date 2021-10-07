ee14ca3396ef11303a02dce8ce96f6c3472ab4f8
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-06 22:31:39 -0700

    Kushol Huq | Object Manipulation 2

--- exercises/object-manipulation-2/src/defaults.js
+++ exercises/object-manipulation-2/src/defaults.js
@@ -1,1 +1,17 @@
 /* exported defaults */
+function defaults(target, source){
+  //if a property exists on source, that target doesnt have
+  //we add it onto target
+
+  //loop through source
+  //on each iteration
+  //check if a source property exists on target
+  //if it does do nothing
+  //if it doesnt add it
+
+  for(property in source){
+    if(target[property]===undefined){
+      target[property] = source[property]
+    }
+  }
+}
