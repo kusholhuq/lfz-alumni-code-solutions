4adb05104139053a3dfe5991676a3553c28bdae9
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-06 22:26:22 -0700

    invert

--- exercises/object-manipulation-2/src/invert.js
+++ exercises/object-manipulation-2/src/invert.js
@@ -1,1 +1,16 @@
 /* exported invert */
+function invert(source){
+  //create two arrays
+  //go through the source object pushing keys to one and values to the other
+  const keys = [];
+  const values = [];
+  for(property in source){
+    keys.push(property);
+    values.push(source[property])
+  }
+  const inverted = {};
+  for(let i=0; i<keys.length; i++){
+    inverted[values[i]]=keys[i];
+  }
+  return inverted;
+}
