58cfe7229dec3e9080b490b06777aa69bcde913e
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-07 13:53:33 -0700

    isAnagram

--- exercises/string-manipulation-3/src/is-palindromic.js
+++ exercises/string-manipulation-3/src/is-palindromic.js
@@ -1,17 +1,6 @@
 /* exported isPalindromic */
 function isPalindromic(string){
-  // for(let i=0; i<string.length; i++){
-  //   if(string[i] !== string[string.length-1-i]){
-  //     return false;
-  //   }
-  // }
-  // return true;
-
-  //reverse the string and compare
-  //make sure to ignore empty spaces
-
-  //remove spaces
-  let spacesRemoved = string.split(' ').join('')
+  let spacesRemoved = string.split(' ').join('');
 
   let reverse = '';
   for (let i = 0; i < spacesRemoved.length;i++){
