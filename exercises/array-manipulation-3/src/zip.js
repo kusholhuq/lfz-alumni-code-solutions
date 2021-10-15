9a4a6efb9623a2605b3d1c3095effeea09dba481
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-14 23:39:05 -0700

    zip

--- exercises/array-manipulation-3/src/zip.js
+++ exercises/array-manipulation-3/src/zip.js
@@ -1,1 +1,15 @@
 /* exported zip */
+function zip(first, second){
+  let array;
+  if(first.length>=second.length){
+    array = second;
+  } else {
+    array = first;
+  }
+  const result = [];
+  for(let i=0; i<array.length; i++){
+    result.push([first[i],second[i]]);
+  }
+  console.log(result)
+  return result;
+}
