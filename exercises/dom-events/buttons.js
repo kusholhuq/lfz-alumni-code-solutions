eafcefa2957d47ce94c44b5dd6232a75b885a39e
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-25 18:05:37 -0700

    Kushol Huq | DOM Events

--- exercises/dom-events/buttons.js
+++ exercises/dom-events/buttons.js
@@ -6,3 +6,21 @@ function handleClick(event){
 
 var clickButton = document.querySelector('.click-button');
 clickButton.addEventListener('click', handleClick);
+
+function handleMouseover(event){
+  console.log("button hovered");
+  console.log(event);
+  console.log(event.target);
+}
+
+var hoverButton = document.querySelector('.hover-button');
+hoverButton.addEventListener('mouseover', handleMouseover);
+
+function handleDoubleClick(event) {
+  console.log("button double-clicked");
+  console.log(event);
+  console.log(event.target);
+}
+
+var doubleClickButton = document.querySelector('.double-click-button');
+doubleClickButton.addEventListener('dblclick', handleDoubleClick);
