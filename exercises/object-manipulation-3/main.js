ea07bbead24484bdff85f5434ab4a127daed13b8
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-27 17:47:24 -0700

    created deck with nested loops, removed some comments and superfluous code

--- exercises/object-manipulation-3/main.js
+++ exercises/object-manipulation-3/main.js
@@ -18,216 +18,6 @@ const players = [
     hand: []
   }
 ];
-const deck = [
-  {
-    rank: 'Ace',
-    suit: 'clubs'
-  },
-  {
-    rank: '2',
-    suit: 'clubs'
-  },
-  {
-    rank: '3',
-    suit: 'clubs'
-  },
-  {
-    rank: '4',
-    suit: 'clubs'
-  },
-  {
-    rank: '5',
-    suit: 'clubs'
-  },
-  {
-    rank: '6',
-    suit: 'clubs'
-  },
-  {
-    rank: '7',
-    suit: 'clubs'
-  },
-  {
-    rank: '8',
-    suit: 'clubs'
-  },
-  {
-    rank: '9',
-    suit: 'clubs'
-  },
-  {
-    rank: '10',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Jack',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Queen',
-    suit: 'clubs'
-  },
-  {
-    rank: 'King',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Ace',
-    suit: 'spades'
-  },
-  {
-    rank: '2',
-    suit: 'spades'
-  },
-  {
-    rank: '3',
-    suit: 'spades'
-  },
-  {
-    rank: '4',
-    suit: 'spades'
-  },
-  {
-    rank: '5',
-    suit: 'spades'
-  },
-  {
-    rank: '6',
-    suit: 'spades'
-  },
-  {
-    rank: '7',
-    suit: 'spades'
-  },
-  {
-    rank: '8',
-    suit: 'spades'
-  },
-  {
-    rank: '9',
-    suit: 'spades'
-  },
-  {
-    rank: '10',
-    suit: 'spades'
-  },
-  {
-    rank: 'Jack',
-    suit: 'spades'
-  },
-  {
-    rank: 'Queen',
-    suit: 'spades'
-  },
-  {
-    rank: 'King',
-    suit: 'spades'
-  },
-  {
-    rank: 'Ace',
-    suit: 'hearts'
-  },
-  {
-    rank: '2',
-    suit: 'hearts'
-  },
-  {
-    rank: '3',
-    suit: 'hearts'
-  },
-  {
-    rank: '4',
-    suit: 'hearts'
-  },
-  {
-    rank: '5',
-    suit: 'hearts'
-  },
-  {
-    rank: '6',
-    suit: 'hearts'
-  },
-  {
-    rank: '7',
-    suit: 'hearts'
-  },
-  {
-    rank: '8',
-    suit: 'hearts'
-  },
-  {
-    rank: '9',
-    suit: 'hearts'
-  },
-  {
-    rank: '10',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Jack',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Queen',
-    suit: 'hearts'
-  },
-  {
-    rank: 'King',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Ace',
-    suit: 'diamonds'
-  },
-  {
-    rank: '2',
-    suit: 'diamonds'
-  },
-  {
-    rank: '3',
-    suit: 'diamonds'
-  },
-  {
-    rank: '4',
-    suit: 'diamonds'
-  },
-  {
-    rank: '5',
-    suit: 'diamonds'
-  },
-  {
-    rank: '6',
-    suit: 'diamonds'
-  },
-  {
-    rank: '7',
-    suit: 'diamonds'
-  },
-  {
-    rank: '8',
-    suit: 'diamonds'
-  },
-  {
-    rank: '9',
-    suit: 'diamonds'
-  },
-  {
-    rank: '10',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Jack',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Queen',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'King',
-    suit: 'diamonds'
-  }
-];
 
 const valueKey = {
   2: 2,
@@ -245,15 +35,30 @@ const valueKey = {
   Ace: 11
 }
 
-const newPile = [];
+
+function createDeck(){
+  const suits = ["clubs", "spades", "hearts", "diamonds"];
+  const ranks = ['Ace', '2','3','4','5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+  const createdDeck = [];
+  for(let i=0; i<ranks.length; i++){
+    for(let b=0; b<suits.length; b++){
+      const card = {
+        rank: ranks[i],
+        suit: suits[b]
+      }
+      createdDeck.push(card);
+    }
+  }
+  return createdDeck;
+}
+
 
 function shuffle(){
   const shuffledDeck =[];
-  const workingDeck = [...deck]
+  const workingDeck = createDeck();
   while(workingDeck.length){
     let number = Math.floor(Math.random()*(workingDeck.length-1));
     let card = workingDeck.splice(number,1);
-    // newPile.push(card[0]);
     shuffledDeck.push(card[0]);
   }
 
