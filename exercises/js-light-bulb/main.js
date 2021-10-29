d67792b3b0d498afcbf5f0d1f0e7f658341ae05b
Mohsinul Huq <kusholhuq@gmail.com>
2021-10-28 22:25:17 -0700

    Kushol Huq | JavaScript Lightbulb

--- exercises/js-light-bulb/main.js
+++ exercises/js-light-bulb/main.js
@@ -2,16 +2,26 @@ let light = false;
 
 const lightbulb = document.querySelector("#lightbulb");
 const background = document.querySelector("#background");
+const inner = document.querySelector("#inner");
+const smallest = document.querySelector("#smallest");
 
-lightbulb.addEventListener("click", function(){
-
+function lightSwitch() {
+console.log("function call");
   light = !light;
-  if(light === false){
+  if (light === false) {
     lightbulb.className = "circle dark-bulb";
     background.className = "container dark-bg";
+    inner.className = "inner dark-inner";
+    smallest.className = "smallest dark-smallest"
   }
-  if(light === true){
+  if (light === true) {
     lightbulb.className = "circle light-bulb";
     background.className = "container light-bg";
+    inner.className = "inner light-inner";
+    smallest.className = "smallest light-smallest"
+
+
   }
-})
+}
+
+lightbulb.addEventListener("click", lightSwitch )
