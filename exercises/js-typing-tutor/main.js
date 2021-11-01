0f7b603849800574f3db8420635684f0f8b4ee69
Mohsinul Huq <kusholhuq@gmail.com>
2021-11-01 02:10:38 -0700

    basic functionality mostly there

--- exercises/js-typing-tutor/main.js
+++ exercises/js-typing-tutor/main.js
@@ -1,3 +1,31 @@
-//take a string
-//turn it into an array
-//
+const spanArray = document.querySelectorAll("span");
+console.log(spanArray);
+
+let currentLetterIndex = 0;
+
+function keyDownHandler(event){
+
+  const currentLetter = spanArray[currentLetterIndex].innerHTML;
+  console.log(event);
+  console.log(event.key);
+  if(event.key === currentLetter){
+    spanArray[currentLetterIndex].classList.remove("underline");
+    spanArray[currentLetterIndex].classList.remove("red");
+    spanArray[currentLetterIndex].classList.add("green");
+    currentLetterIndex++;
+    if (currentLetterIndex >= spanArray.length) {
+      console.log("all done");
+      document.removeEventListener('keydown', keyDownHandler);
+      return;
+    }
+    spanArray[currentLetterIndex].classList.add("underline");
+    console.log("not what i want");
+
+  } else if(event.key !== currentLetter){
+    spanArray[currentLetterIndex].classList.add("red");
+  }
+
+
+}
+
+document.addEventListener('keydown', keyDownHandler);
