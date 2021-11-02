bc57323ead1de3406ee2b1d1030d583b4b36758f
Mohsinul Huq <kusholhuq@gmail.com>
2021-11-01 17:16:34 -0700

    almost working

--- exercises/js-typing-tutor/main.js
+++ exercises/js-typing-tutor/main.js
@@ -1,10 +1,35 @@
 const spanArray = document.querySelectorAll("span");
-console.log(spanArray);
+const modal = document.querySelector("#modal");
+const accuracy = document.querySelector("#accuracy");
+const playAgainButton = document.querySelector("#play-again");
+
+playAgainButton.addEventListener('click', playAgain);
+document.addEventListener('keydown', keyDownHandler);
 
 let currentLetterIndex = 0;
 let mistakeCounter = 0;
 
+function endGame() {
+  let accuracyScore = Math.floor((mistakeCounter / spanArray.length) * 100);
+  accuracy.textContent = accuracyScore;
+  modal.classList.remove("hidden");
+  document.removeEventListener('keydown', keyDownHandler);
+  console.log("endgame called");
+}
+
+function playAgain() {
+  currentLetterIndex = 0;
+  mistakeCounter = 0;
+  spanArray[0].className = "underline";
+  for (let i = 1; i < spanArray.length; i++) {
+    spanArray[i].className = "";
+  }
+  document.addEventListener('keydown', keyDownHandler);
+  modal.classList.add("hidden");
+}
+
 function keyDownHandler(event){
+  console.log("still handling keydowns")
   if(event.repeat){
     return;
   }
@@ -12,37 +37,18 @@ function keyDownHandler(event){
     return;
   }
   const currentLetter = spanArray[currentLetterIndex].textContent;
-  console.log(event);
-  console.log(event.key);
   if (event.key === currentLetter || (event.code === 'Space' && spanArray[currentLetterIndex].innerHTML ==="&nbsp;")){
     spanArray[currentLetterIndex].classList.remove("underline");
     spanArray[currentLetterIndex].classList.remove("red");
     spanArray[currentLetterIndex].classList.add("green");
     currentLetterIndex++;
     if (currentLetterIndex >= spanArray.length) {
-      console.log("all done");
-      document.removeEventListener('keydown', keyDownHandler);
+      endGame();
       return;
     }
     spanArray[currentLetterIndex].classList.add("underline");
-    console.log("not what i want");
-
   } else if(event.key !== currentLetter){
     spanArray[currentLetterIndex].classList.add("red");
     mistakeCounter++;
   }
-
-
-}
-
-document.addEventListener('keydown', keyDownHandler);
-
-function playAgain(){
-  currentLetterIndex = 0;
-  mistakeCounter = 0;
-  spanArray[0].className = "underline";
-  for(let i=1; i<spanArray.length; i++){
-    spanArray[i].className = "";
-  }
-  document.addEventListener('keydown', keyDownHandler);
 }
