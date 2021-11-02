116ed5de106f5a4edd4eeb5f462295ea6413c724
Mohsinul Huq <kusholhuq@gmail.com>
2021-11-01 17:36:36 -0700

    accuracy calculation fixed

--- exercises/js-typing-tutor/main.js
+++ exercises/js-typing-tutor/main.js
@@ -7,10 +7,11 @@ playAgainButton.addEventListener('click', playAgain);
 document.addEventListener('keydown', keyDownHandler);
 
 let currentLetterIndex = 0;
-let mistakeCounter = 0;
+let keydownCounter = 0;
+let correctKeydownCounter = 0
 
 function endGame() {
-  let accuracyScore = 100 - Math.floor((mistakeCounter / spanArray.length) * 100);
+  let accuracyScore = Math.floor((correctKeydownCounter / keydownCounter) * 100);
   accuracy.textContent = accuracyScore;
   modal.classList.remove("hidden");
   document.removeEventListener('keydown', keyDownHandler);
@@ -18,7 +19,8 @@ function endGame() {
 
 function playAgain() {
   currentLetterIndex = 0;
-  mistakeCounter = 0;
+  keydownCounter = 0;
+  correctKeydownCounter = 0
   spanArray[0].className = "underline";
   for (let i = 1; i < spanArray.length; i++) {
     spanArray[i].className = "";
@@ -34,12 +36,14 @@ function keyDownHandler(event){
   if(event.key==="Shift"){
     return;
   }
+  keydownCounter++;
   const currentLetter = spanArray[currentLetterIndex].textContent;
   if (event.key === currentLetter || (event.code === 'Space' && spanArray[currentLetterIndex].innerHTML ==="&nbsp;")){
     spanArray[currentLetterIndex].classList.remove("underline");
     spanArray[currentLetterIndex].classList.remove("red");
     spanArray[currentLetterIndex].classList.add("green");
     currentLetterIndex++;
+    correctKeydownCounter++;
     if (currentLetterIndex >= spanArray.length) {
       endGame();
       return;
@@ -47,6 +51,5 @@ function keyDownHandler(event){
     spanArray[currentLetterIndex].classList.add("underline");
   } else if(event.key !== currentLetter){
     spanArray[currentLetterIndex].classList.add("red");
-    mistakeCounter++;
   }
 }
