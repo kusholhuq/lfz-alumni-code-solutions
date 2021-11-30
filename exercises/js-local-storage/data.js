f8d684915b24c148ebab13ffb412c9faa742adbf
Mohsinul Huq <kusholhuq@gmail.com>
2021-11-30 00:10:32 -0800

    Kushol Huq | JavaScript Local Storage

--- exercises/js-local-storage/data.js
+++ exercises/js-local-storage/data.js
@@ -1,3 +1,16 @@
 /* exported todos */
 
 var todos = [];
+
+const previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if(previousTodosJSON !== null){
+  todos = JSON.parse(previousTodosJSON);
+}
+
+const beforeUnloadListener = (event) => {
+  event.preventDefault();
+  const todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnloadListener);
