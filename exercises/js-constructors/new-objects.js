1dced915bfd4dd2284a175ed45f4e96db45e13a9
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-11 21:03:20 -0800

    new

---
+++ exercises/js-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var anObject = new Object();
+console.log("anObject value: ", anObject);
+console.log("anObject type: ", typeof anObject);
+
+var anArray = new Array();
+console.log("anArray value: ", anArray);
+console.log("anArray type: ", typeof anArray);
+
+var aFunction = new Function();
+console.log("aFunction value: ", aFunction);
+console.log("aFunction type: ", typeof aFunction);
+
+var aString = new String();
+console.log("aString value: ", aString);
+console.log("aString type: ", typeof aString);
+
+var aNumber = new Number();
+console.log("aNumber value: ", aNumber);
+console.log("aNumber type: ", typeof aNumber);
+
+var aBoolean = new Boolean();
+console.log("aBoolean value: ", aBoolean);
+console.log("aBoolean type: ", typeof aBoolean);
