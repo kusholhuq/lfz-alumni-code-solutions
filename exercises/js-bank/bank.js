1485a857595d4c3257e878d8bbbf900f46677fae
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-17 02:32:31 -0800

    start of bank

--- exercises/js-bank/bank.js
+++ exercises/js-bank/bank.js
@@ -1,1 +1,17 @@
 /* exported Bank */
+function Bank () {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+
+}
+
+Bank.prototype.getAccount = function (number) {
+
+}
+
+Bank.prototype.getTotalAssets = function () {
+
+}
