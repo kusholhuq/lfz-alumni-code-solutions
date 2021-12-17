0f6f506dc12d75041a09003141d669a344a65543
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-17 01:55:12 -0800

    accounts

--- exercises/js-bank/account.js
+++ exercises/js-bank/account.js
@@ -7,20 +7,37 @@ function Account (number, holder) {
 
 Account.prototype.deposit = function (amount) {
   if (amount > 0){
-
+    const depositTransaction = new Transaction('deposit', amount);
+    this.transactions.push(depositTransaction);
+    return true;
   } else {
     return false;
   }
-  const depositTransaction = new Transaction('deposit', amount );
-  this.transactions.push(depositTransaction);
-  return true;
 
 }
 
-Account.prototype.withdraw = function () {
-
+Account.prototype.withdraw = function (amount) {
+  if (amount > 0) {
+    const withdrawalTransaction = new Transaction('withdraw', amount);
+    this.transactions.push(withdrawalTransaction);
+    return true;
+  } else {
+    return false;
+  }
 }
 
 Account.prototype.getBalance = function () {
-
+  if(this.transactions.length === 0){
+    return 0
+  }
+  let balance = 0;
+  for(let i=0; i<this.transactions.length; i++){
+    if(this.transactions[i].type === "deposit"){
+      balance += this.transactions[i].amount
+    }
+    if (this.transactions[i].type === "withdraw") {
+      balance -= this.transactions[i].amount
+    }
+  }
+  return balance;
 }
