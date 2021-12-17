a40423402e23e8a3bef04deeef7694d5a56a155b
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-16 23:09:07 -0800

    deposit

--- exercises/js-bank/account.js
+++ exercises/js-bank/account.js
@@ -1,4 +1,26 @@
 /* exported Account */
 function Account (number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (amount > 0){
+
+  } else {
+    return false;
+  }
+  const depositTransaction = new Transaction('deposit', amount );
+  this.transactions.push(depositTransaction);
+  return true;
+
+}
+
+Account.prototype.withdraw = function () {
+
+}
+
+Account.prototype.getBalance = function () {
 
 }
