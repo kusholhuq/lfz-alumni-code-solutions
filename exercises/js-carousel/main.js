aee0f6e038077b13cb328603677779bc670f78f5
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-25 00:01:40 -0800

    break not working

--- exercises/js-carousel/main.js
+++ exercises/js-carousel/main.js
@@ -11,3 +11,23 @@ const imageHolder = document.querySelector("#picture");
 const image = document.createElement("img");
 image.setAttribute("src","./images/"+imageArray[0]);
 imageHolder.appendChild(image);
+
+let switched = false;
+let counter = 1
+
+function autoSwitch () {
+  if(switched){
+    switched = false;
+    return;
+  }
+  if(counter > 4){
+    counter = 0;
+  }
+  imageHolder.removeChild(imageHolder.firstChild)
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", "./images/" + imageArray[counter]);
+  console.log("./images/" + imageArray[counter])
+  counter++;
+}
+
+setInterval(autoSwitch, 3000);
