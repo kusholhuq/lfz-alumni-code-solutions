b76dc60e0d069628f3446ab3ef83a69c8027cd7c
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-24 23:43:39 -0800

    skeleton complete

--- exercises/js-carousel/main.js
+++ exercises/js-carousel/main.js
@@ -5,3 +5,9 @@
 //the dots reflect the array.length
 
 const imageArray = ["001.png", "004.png", "007.png", "025.png", "039.png"];
+
+const imageHolder = document.querySelector("#picture");
+
+const image = document.createElement("img");
+image.setAttribute("src","./images/"+imageArray[0]);
+imageHolder.appendChild(image);
