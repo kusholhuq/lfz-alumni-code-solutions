ee9ac58d3c6083e7f04aa5443bfdb9363c707700
Mohsinul Huq <kusholhuq@gmail.com>
2021-12-24 23:12:37 -0800

    starter files

---
+++ exercises/js-carousel/main.js
@@ -0,0 +1,7 @@
+//solution
+//brainstorm
+//an array containing the image source
+//every three seconds it goes to the next index
+//the dots reflect the array.length
+
+const imageArray = ["001.png", "004.png", "007.png", "025.png", "039.png"];
